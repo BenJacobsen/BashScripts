45642a37b96e0b1fae1aa468eb03c028223eca43
bjacobsen <32605341+Bizzebgee@users.noreply.github.com>
2019-11-21 22:22:35 -0500

    Add option parsing skeleton

--- nav.sh
+++ nav.sh
@@ -1,7 +1,20 @@
-!#/usr/bin/bash
+#!/usr/bin/env bash
 # nav is used to cd to manage and use keywords that are associated with directories
 # remember: (1) run in source  (2) reserve -* for options
 # use:
-#. navh[keyword] - go to dir of keyword
-#. nav.sh -s [keyword] [dir] - set keyword and dir in nav.txt
+# . nav.sh [keyword] - go to dir of keyword
+# . nav.sh -s [keyword] [dir] - set keyword and dir in nav.txt
 
+if [[ ${$1:0:1} == "-" ]] ; then
+    case ${$1:1:${#$1}-1} in
+        s)
+#            if [[ ! -f $PWD/nav.txt ]] ; then
+#                touch $PWD/nav.txt
+#            fi
+            echo "-s"
+        ;;
+        *)
+            echo "Unrecognized option"
+        ;;
+    esac
+fi
