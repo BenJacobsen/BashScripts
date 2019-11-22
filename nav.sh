3835ddf508ac7a28ff2ab9658e0df8990e8ab9a8
bjacobsen <32605341+Bizzebgee@users.noreply.github.com>
2019-11-21 21:42:22 -0500

    add nav.sh with usage info

---
+++ nav.sh
@@ -0,0 +1,7 @@
+!#/usr/bin/bash
+# nav is used to cd to manage and use keywords that are associated with directories
+# remember: (1) run in source  (2) reserve -* for options
+# use:
+#. navh[keyword] - go to dir of keyword
+#. nav.sh -s [keyword] [dir] - set keyword and dir in nav.txt
+
