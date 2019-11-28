40977dd0c085ef48c41aed211395ebc09e5fe612
bjacobsen <32605341+Bizzebgee@users.noreply.github.com>
2019-11-27 21:13:13 -0500

    -s set new_dir

--- nav.sh
+++ nav.sh
@@ -5,15 +5,22 @@
 # . nav.sh [keyword] - go to dir of keyword
 # . nav.sh -s [keyword] [dir] - set keyword and dir in nav.txt
 # . nav.sh -l - list all keywords and corresponding dirs
+new_dir=''
 while getopts "s:l:" op; do
     case "${op}" in
         s)
-            DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-            if [[ ! -f $DIR/nav.txt ]] ; then
-                touch $DIR/nav.txt
+            source_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+            if [[ ! -f $source_dir/list_nav.txt ]] ; then
+                touch $source_dir/list_nav.txt
+            fi
+            if [ -d "${OPTARG}" ]; then
+                new_dir="${OPTARG}"
+            else
+                echo "ERROR: ${OPTARG} is not a directory" 1>&2
             fi
         ;;
         l)
+
         ;;
         *)
             echo "Usage: . $0 [-s dir] [-l list] [keyword]" 1>&2 
